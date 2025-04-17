b92883f22825af4ad92d5fd9a7fc774e1f187fa0
jdluis <jdluisdev@gmail.com>
2025-04-17 09:06:37 +0200

    App working in same origin

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { logEvent } from "firebase/analytics";
+import { analytics } from "../firebaseConfig.ts";
+
+logEvent(analytics, 'app_started');
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
